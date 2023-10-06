18117197500fb1182aea97eec485c53d9a92d48d
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-10-06 17:56:19 +0200

    don't remember what i heave done

--- js/index.js
+++ js/index.js
@@ -16,6 +16,16 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  
+  // const checkBox = event.target.elements.tos
+  const formData = new FormData(event.target)
+  const formObject = Object.fromEntries(formData)
+  console.log("event",event);
+  console.log("target", event.target)
+  console.log("elemets", event.target.elements)
+  console.log(event.target.elements.tos)
+  console.log(event.target.elements.tos.checked)
+  // console.log(for)
 
   // --^-- write your code here --^--
 
